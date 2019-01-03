3772be75e9920b681ce61a05ddaafd8a03959ccb
rakhshimalik <44527369+rakhshimalik@users.noreply.github.com>
2019-01-03 23:22:37 +0500

    Add files via upload

---
+++ database/insert_product.php
@@ -0,0 +1,186 @@
+<?php
+$con=mysqli_connect("localhost","root","","data");
+if(!$con){
+    die("connection Failed");
+    echo "hi";
+
+
+
+    require_once "db_connection.php";
+    if(isset($_post['insert_pro']))
+    {
+        $pro_title = $_post['pro_title'];
+        $pro_cat = $_post['pro_cat'];
+        $pro_brand = $_post['pro_brand'];
+        $pro_price = $_post['pro_price'];
+        $pro_desc = $_post['pro_desc'];
+        $pro_keywords = $_post['pro_kw'];
+
+        $insertQuery = "insert into products (`pro_title`,`pro_cat`,`pro_brand`,`pro_price`,`pro_desc`,`pro_keywords`)
+        VALUES ('$pro_title','$pro_cat','$pro_brand','$pro_price','$pro_desc','$pro_kw')";
+
+
+        $result = mysqli_query($con,$insertQuery);
+
+        if(!$result)
+        {
+            echo "not Executed";
+        }
+
+    }
+
+    //   this  is display
+    $sql = "SELECT pro_title, pro_cat, pro_brand,pro_price,pro_desc,pro_keywords FROM Tech Box";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while($row = $result->fetch_assoc()) {
+
+            echo "pro_title: " . $row["pro_title"]. " - pro_cat: " . $row["pro_cat"]. " " ." - pro_brand: " . $row["pro_brand"]. " - pro_price: " . $row["pro_price"]. " - pro_desc: " . $row["pro_desc"]. " - pro_keywords: " . $row["pro_keywords"]  . "<br>";
+        }
+    }
+    else {
+        echo "0 results";
+    }
+
+    $conn->close();
+
+
+
+
+
+
+
+
+
+
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Insert Product</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <link rel="stylesheet" href="../css/bootstrap.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Bangers|Old+Standard+TT">
+    <style>
+        * {
+            font-family: 'Old Standard TT', serif;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <h1 class="text-center my-4"><i class="fas fa-plus fa-md"></i> <span class="d-none d-sm-inline"> Add New </span> Product </h1>
+    <form>
+        <div class="row">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_title" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span> Title:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-file-signature"></i></div>
+                    </div>
+                    <input type="text" class="form-control" id="pro_title" name="pro_title" placeholder="Enter Product Title" >
+                </div>
+            </div>
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_cat" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Category:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-3 mt-lg-0">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-list-alt"></i></div>
+                    </div>
+                    <select class="form-control" id="pro_cat" name="pro_cat">
+                        <option>Select Category</option>
+                        <?php
+                        require "../db_connection1.php";
+                        include "../function.php";
+                        getCats1();
+                        ?>
+                    </select>
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_brand" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span> Brand:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-stamp"></i></div>
+                    </div>
+                    <select class="form-control" id="pro_brand" name="pro_brand">
+                        <option>Select Brand</option>
+                        <?php
+                        getBrands1();
+                        ?>
+                    </select>
+                </div>
+            </div>
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_img" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Image:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-3 mt-lg-0">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="far fa-image"></i></div>
+                    </div>
+                    <input class="form-control" type="file" id="pro_img" name="pro_img">
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_price" class="float-md-right"> <span class="d-sm-none d-md-inline"> Product </span> Price:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-money-bill"></i></div>
+                    </div>
+                    <input class="form-control" id="pro_price" name="pro_price" placeholder="Enter Product Price">
+                </div>
+            </div>
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_kw" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Keyword:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4 mt-3 mt-lg-0">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="fas fa-key"></i></div>
+                    </div>
+                    <input class="form-control" type="text" id="pro_kw" name="pro_kw" placeholder="Enter Product Keywords">
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto">
+                <label for="pro_desc" class="float-md-right"><span class="d-sm-none d-md-inline"> Product </span> Detail:</label>
+            </div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <div class="input-group">
+                    <div class="input-group-prepend">
+                        <div class="input-group-text"><i class="far fa-comment-alt"></i></div>
+                    </div>
+                    <textarea class="form-control" type="file" id="pro_desc" name="pro_desc" placeholder="Enter Product Detail"></textarea>
+                </div>
+            </div>
+        </div>
+        <div class="row my-3">
+            <div class="d-none d-sm-block col-sm-3 col-md-4 col-lg-2 col-xl-2 mt-auto"></div>
+            <div class="col-sm-9 col-md-8 col-lg-4 col-xl-4">
+                <button type="submit" class="btn btn-primary btn-block"><i class="fas fa-plus"></i> Insert Now </button>
+            </div>
+        </div>
+    </form>
+</div>
+</body>
+</html>
